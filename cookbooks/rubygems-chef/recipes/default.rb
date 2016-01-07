5f16075f8fad77f343d55629d26d99d3b22f3f6f
mzaideel <mozaa84@gmail.com>
2016-01-08 03:07:26 +0800

    updated

--- cookbooks/rubygems-chef/recipes/default.rb
+++ cookbooks/rubygems-chef/recipes/default.rb
@@ -3,9 +3,6 @@
 # Recipe:: default
 #
 
-#include_recipe 'rubygems-chef::librato'
-#include_recipe 'rubygems-chef::slack'
-
 node.default['chef_client']['config']['ssl_verify_mode'] = ':verify_peer'
 node.default['chef_client']['config']['client_fork'] = true
 
@@ -19,6 +16,6 @@ service 'chef-client' do
   action [:stop, :disable]
 end
 
-node.default['omnibus_updater']['version'] = '11.16.2'
+node.default['omnibus_updater']['version'] = '12.6.0'
 node.default['omnibus_updater']['restart_chef_service'] = true
 include_recipe 'omnibus_updater'
--- cookbooks/rubygems-logging/recipes/default.rb
+++ cookbooks/rubygems-logging/recipes/default.rb
@@ -3,13 +3,7 @@
 # Recipe:: default
 #
 
-include_recipe 'chef-vault'
-
-papertrail_creds = chef_vault_item('papertrail', 'credentials')
-
-node.default['rsyslog']['server_ip'] = papertrail_creds['server']
-node.default['rsyslog']['port'] = papertrail_creds['port']
+node.default['rsyslog']['server_ip'] = log_server['ipaddress']
+node.default['rsyslog']['port'] = '5959'
 node.default['rsyslog']['preserve_fqdn'] = 'on'
 node.default['rsyslog']['high_precision_timestamps'] = true
-
-include_recipe 'rsyslog::client'
